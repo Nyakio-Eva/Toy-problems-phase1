34dd6250bee90a37b93bbb08c479e0a72489fd16
Nyakio-Eva <evalkariuki@gmail.com>
2023-12-18 10:14:15 +0300

    modified and edited  the sol1.js file

--- mysolutions/sol1.js
+++ mysolutions/sol1.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+
 //function that generates grade according to the user's input
 function generateGrade(){
     const rl = readline.createInterface({
@@ -7,25 +8,22 @@ function generateGrade(){
     });
   //prompt the user for input
   let usermarks;
-    rl.question('Enter student marks (between 0 and 100):' , (usermarks)) => {
-        //validate user input
+    rl.question('Enter student marks (between 0 and 100):', (usermarks) => {
+        //validate user input with if statement
+        //inform the user to enter valid input
         if(isNaN (usermarks)){
             console.log('Invalid input. Please enter a number!');
             rl.close();
             return;
         }
-        if(!Number.isInteger(usermarks)){
-            console.log('Decimals not allowed. Please enter a whole number!');
-            rl.close();
-            return;
-        }
-        //validate input range
+        //validate input range with if statement
+        //inform the user to input a number within the range
         if(usermarks < 0 || usermarks > 100){
             console.log('Invalid input. Please enter marks between o and 100.');
             rl.close();
             return;
         }
-        //generate the grade
+        //generate the grade by comparing the numbers entered and the grading criteria
         let grade;
         if(usermarks > 79){
             grade = 'A';
@@ -43,10 +41,10 @@ function generateGrade(){
             grade = 'E';
         }
         //display the grade = output
-        console.log('The grade for ${usermarks} marks is: ${grade})');
+        console.log(`The grade for ${usermarks} marks is: ${grade})`);
         rl.close();
 
-    };
+    });
 }
 //call the function
 generateGrade();
